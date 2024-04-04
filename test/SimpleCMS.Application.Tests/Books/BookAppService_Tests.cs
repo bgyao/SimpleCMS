680772f5645c357cd00e7bd2dbcb86e22fae159f
Bryan Jerahmeel Yao <bgyao@live.com>
2024-04-04 19:19:02 +0800

    Book APIs Integration Tests
    Integration Tests include:
    - getting a list of books
    - creating a valid book
    - creating a book without required fields (Name) will result to an error
    - creating a book with Name exceeding max character limit (1280 will result to an error

---
+++ test/SimpleCMS.Application.Tests/Books/BookAppService_Tests.cs
@@ -0,0 +1,95 @@
+﻿using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.Modularity;
+using Volo.Abp.Validation;
+using Xunit;
+
+namespace SimpleCMS.Books;
+
+public abstract class BookAppService_Tests<TStartupModule> :
+    SimpleCMSApplicationTestBase<TStartupModule>
+    where TStartupModule : IAbpModule
+{
+    private readonly IBookAppService _bookAppService;
+
+    protected BookAppService_Tests()
+    {
+        _bookAppService = GetRequiredService<IBookAppService>();
+    }
+
+    [Fact]
+    public async Task Should_Get_List_Of_Books()
+    {
+        //Act
+        var result = await _bookAppService.GetListAsync(
+            new PagedAndSortedResultRequestDto() );
+
+        //Assert
+        result.TotalCount.ShouldBeGreaterThan(0);
+        result.Items.ShouldContain(b => b.Name == "1984");
+    }
+
+    [Fact]
+    public async Task Should_Create_A_Valid_Book()
+    {
+        //Act
+        var result = await _bookAppService.CreateAsync(
+            new Dtos.CreateUpdateBookDto
+            {
+                Name = "New test book 42",
+                Price = 10,
+                PublishDate = DateTime.Now,
+                Type = BookType.ScienceFiction
+            });
+
+        //Assert
+        result.Id.ShouldNotBe(Guid.Empty);
+        result.Name.ShouldBe("New test book 42");
+    }
+
+    [Fact]
+    public async Task Should_Not_Create_A_Book_Without_Name()
+    {
+        var ex = await Assert.ThrowsAsync<AbpValidationException>( async() =>
+        {
+            await _bookAppService.CreateAsync(
+                new Dtos.CreateUpdateBookDto
+                {
+                    Name = "",
+                    Price = 10,
+                    PublishDate = DateTime.Now,
+                    Type = BookType.ScienceFiction
+                }
+                );
+        });
+
+        ex.ValidationErrors
+            .ShouldContain(err => err.MemberNames.Any(mem => mem == "Name"));
+    }
+
+    // Not included in the tutorial. My addition. Name has 129 characters
+    [Fact]
+    public async Task Should_Not_Create_A_Book_With_Name_More_Than_Max_Char_Limit()
+    {
+        var ex = await Assert.ThrowsAsync<AbpValidationException>(async () =>
+        {
+            await _bookAppService.CreateAsync(
+                new Dtos.CreateUpdateBookDto
+                {
+                    Name = "The quick brown fox jumps over the lazy dog because the dog is too lazy to move and the turtle beats the rabbit in a mockup race!",
+                    Price = 10,
+                    PublishDate = DateTime.Now,
+                    Type = BookType.ScienceFiction
+                }
+                );
+        });
+
+        ex.ValidationErrors
+            .ShouldContain(err => err.MemberNames.Any(mem => mem == "Name"));
+    }
+}
