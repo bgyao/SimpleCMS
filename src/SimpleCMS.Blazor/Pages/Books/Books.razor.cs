1d9b93e5fbf4d9ce1b922277e1e216d87912fb65
Bryan Jerahmeel Yao <bgyao@live.com>
2024-04-05 14:16:47 +0800

    Bug Fixes
    - Books UI: fixed bug that causes error (Cannot access disposed object)
    - Author UI: added missing Short Bio column
    - Localization: added localized string for ShortBio column name

---
+++ src/SimpleCMS.Blazor/Pages/Books/Books.razor.cs
@@ -0,0 +1,38 @@
+﻿using SimpleCMS.Books.Dtos;
+using SimpleCMS.Permissions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Volo.Abp;
+
+namespace SimpleCMS.Blazor.Pages.Books;
+
+public partial class Books
+{
+    IReadOnlyList<AuthorLookupDto> authorList = Array.Empty<AuthorLookupDto>();
+
+    public Books()
+    {
+        CreatePolicyName = SimpleCMSPermissions.Books.Create;
+        UpdatePolicyName = SimpleCMSPermissions.Books.Edit;
+        DeletePolicyName = SimpleCMSPermissions.Books.Delete;
+    }
+
+    protected override async Task OnInitializedAsync()
+    {
+        await base.OnInitializedAsync();
+        authorList = (await AppService.GetAuthorLookupAsync()).Items;
+    }
+
+    protected override async Task OpenCreateModalAsync()
+    {
+        if (!authorList.Any())
+        {
+            throw new UserFriendlyException(message: L["AnAuthorIsRequiredForCreatingBook"]);
+        }
+
+        await base.OpenCreateModalAsync();
+        NewEntity.AuthorId = authorList.First().Id;
+    }
+}
