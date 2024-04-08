b152cb427f03933326ba50779383e9cf4c25e231
Bryan Jerahmeel Yao <bgyao@live.com>
2024-04-08 12:21:19 +0800

    Created EditArticle.razor.cs

---
+++ src/SimpleCMS.Blazor/Pages/Articles/Edit/EditArticle.razor.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Components;
+using SimpleCMS.CmsContents;
+using SimpleCMS.CmsContents.Dtos;
+using System;
+using System.Threading.Tasks;
+
+namespace SimpleCMS.Blazor.Pages.Articles.Edit;
+
+public partial class EditArticle
+{
+    public EditArticle() { }
+
+    [Parameter]
+    public Guid Id { get; set; }
+    
+    [Inject]
+    public ICmsContentAppService CmsContentAppService { get; set; }
+
+    public CmsContentDto Article { get; set; }
+    public bool IsLoading = true;
+
+    protected override async Task OnInitializedAsync()
+    {
+        try
+        {
+            Article = await CmsContentAppService.GetAsync(Id);
+            IsLoading = false;
+            StateHasChanged();
+        }
+        catch (Exception ex)
+        {
+            //await _js.InvokeVoidAsync("defaultMessage", "error", "Failed to load", ex.Message);
+            Console.WriteLine(ex.Message);
+        }
+    }
+}
